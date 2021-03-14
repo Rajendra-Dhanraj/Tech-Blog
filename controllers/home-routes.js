295cb9ac8ec50d3c8dbda4dc08bda521a720e772
Rajendra Dhanraj <rajiedhan@Rajendras-MacBook-Pro.local>
2021-03-14 03:03:05 -0400

    login and log out with sessions

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,19 +3,6 @@ const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
 // const { route } = require("./api"); ??
 
-// router.get("/", (req, res) => {
-//   res.render("homepage", {
-//     id: 1,
-//     title: "this is the damn title",
-//     post_text: "I am fed up",
-//     created_at: new Date(),
-//     comments: [{}, {}],
-//     user: {
-//       username: "test_user",
-//     },
-//   });
-// });
-
 router.get("/", (req, res) => {
   Post.findAll({
     attributes: ["id", "post_text", "title", "created_at"],
@@ -48,12 +35,10 @@ router.get("/", (req, res) => {
 });
 
 router.get("/login", (req, res) => {
-  // check to see if user is logged in
   if (req.session.loggedIn) {
     res.redirect("/");
     return;
   }
-
   res.render("login");
 });
 
