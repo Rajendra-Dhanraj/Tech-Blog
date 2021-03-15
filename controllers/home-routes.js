3410fbf905361729b3294e74b1d96db1434cf3e8
Rajendra Dhanraj <rajiedhan@Rajendras-MacBook-Pro.local>
2021-03-14 21:27:24 -0400

    separate handlebars for log in and sign up

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -45,6 +45,10 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 
+router.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
 router.get("/", (req, res) => {
   res.render("homepage");
 });
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,29 +1,3 @@
-async function signupFormHandler(event) {
-  event.preventDefault();
-
-  const username = document.querySelector("#username-signup").value.trim();
-  const password = document.querySelector("#password-signup").value.trim();
-
-  if (username && password) {
-    // when using await, promise can be assigned to a variable, therefor a "catch" or "then" is not needed!
-    const response = await fetch("/api/users", {
-      method: "post",
-      body: JSON.stringify({
-        username,
-        password,
-      }),
-      headers: { "Content-Type": "application/json" },
-    });
-    // check the response status
-    if (response.ok) {
-      console.log("success");
-      document.location.replace("/");
-    } else {
-      alert(response.statusText);
-    }
-  }
-}
-
 async function loginFormHandler(event) {
   event.preventDefault();
 
@@ -51,7 +25,3 @@ async function loginFormHandler(event) {
 document
   .querySelector(".login-form")
   .addEventListener("submit", loginFormHandler);
-
-document
-  .querySelector(".signup-form")
-  .addEventListener("submit", signupFormHandler);
